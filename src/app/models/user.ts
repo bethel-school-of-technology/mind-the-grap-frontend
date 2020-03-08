99d0b75d48e9fb845b3e4727aeb78c9be7121127
taylor davis <thecatsmeow584@gmail.com>
2020-03-08 18:32:46 -0500

    Message stating what was changed

---
+++ src/app/models/user.ts
@@ -0,0 +1,10 @@
+export class User {
+    id: number;
+    userName: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    gender: string;
+    dateOfBirth: Date;
+    password: string;
+}
---
+++ src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user';
+
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent implements OnInit {
+  model: User = new User();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+//added console log for User 03.08.20//
+  onSubmit() {
+    console.log('Submit Successful: ', this.model);
+}
+
+}
