2df23470e369fba09f9beac2090981b1db52721d
taylor davis <thecatsmeow584@gmail.com>
2020-03-14 14:58:33 -0500

    fixing master conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { QuestionListComponent } from './question-list/question-list.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-import { FormsModule } from '@angular/forms';
-=======
 import { QuestionListComponent } from './components/question-list/question-list.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { QuestionsComponent } from './components/questions/questions.component';
 import { LoginComponent } from './components/login/login.component';
->>>>>>> master
+
 
 @NgModule({
   declarations: [
@@ -21,14 +16,11 @@ import { LoginComponent } from './components/login/login.component';
     QuestionListComponent,
     SignUpComponent,
     QuestionsComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-<<<<<<< HEAD
-=======
-    HttpClientModule, 
->>>>>>> master
+    HttpClientModule,
     AppRoutingModule,
     FormsModule
   ],
