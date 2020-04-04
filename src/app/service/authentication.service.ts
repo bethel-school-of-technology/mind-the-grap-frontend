8459b15b362ada1fd004a98963b3611320ad65cf
taylor davis <thecatsmeow584@gmail.com>
2020-04-04 17:34:56 -0500

    Ball is life. life is ball
    praise the Lord

--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { 
+    
+  }
 }
